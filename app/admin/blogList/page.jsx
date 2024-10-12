a67e0b21b14557c358193bf440a46a56f2fab21d
kumar chaudhary <chaudharykumar228@gmail.com>
2024-10-12 12:24:39 +0530

    Code Updated

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>bloglist page</div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+    <h1>All blogs</h1>
+    <div className='relative h-[80vh]max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'></div>
+    </div>
   )
 }
 
--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,3 +1,4 @@
+import BlogList from "@/Components/BlogList";
 import { ConnectDB } from "@/lib/config/db";
 import BlogModel from "@/lib/models/BlogModel";
 import {writeFile} from 'fs/promises';
@@ -10,32 +11,41 @@ const LoadDB=async()=>{
 }
 
 LoadDB();
-
+//API Endpoint for getting all the blogs
 export async function GET(request) {
-  
+  const blogId = request.nextUrl.searchParams.get("id");
+  if(blogId){
+    const blog=await BlogModel.findById(blogId);
 
-  return NextResponse.json({message:"API Working"})
+    return NextResponse.json(blog)
+  }
+  else{
+    const blogs =await BlogModel.find({});
+  
+    return NextResponse.json({blogs})
+  }
 }
 
+//API Endpoint for uploading the blog
 export async function POST(request) {
 
   const formData = await request.formData();
-  // const timestamp = Date.now();
-  // const image = formData.get('image');
+  const timestamp = Date.now();
+  const image = formData.get('image');
 
-  // const imageByteData = await image.arrayBuffer();
-  // const buffer = Buffer.from(imageByteData);
-  // const path = `./public/${timestamp}_${image.name}`;
-  // await writeFile(path,buffer);
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path,buffer);
 
-  // const imgUrl = `/${timestamp}_${image.name}`;
+  const imgUrl = `/${timestamp}_${image.name}`;
   
   const blogData = {
     title:`${formData.get('title')}`,
     description:`${formData.get('description')}`,
     category:`${formData.get('category')}`,
     author:`${formData.get('author')}`,
-    // image:`${imgUrl}`,
+    image:`${imgUrl}`,
     authorImg:`${formData.get('authorImg')}`
   }
   await BlogModel.create(blogData);
